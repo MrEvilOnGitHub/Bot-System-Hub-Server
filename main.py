8646845cef79c168ce5e635bfec7e166d2da096e
MrEvilOnGitHub <beckersamuel9@gmail.com>
2021-02-12 12:09:46 +0100

    nuke testing stuff, set up for initial use

--- main.py
+++ main.py
@@ -1,53 +1,16 @@
-# This will be the main server for general purpose activities of the bots, 
-# like checking against banned phrases, sharing data, etc
-
 import flask
+import sqlite3
 
 app = flask.Flask(__name__)
-app.config["DEBUG"] = True
 
-some_data = [
-    {
-        "value1": "key1",
-        "value2": "key2",
-        "id": 0
-    }, 
-    {
-        "value3": "value3",
-        "value4": "value4",
-        "id": 1
-    }
-]
+#Enable debug mode for now. Disable later once live
+app.config['DEBUG'] = True
 
-@app.errorhandler(404)
-def page_not_found():
-    return '<h1>ERROR 404</h1><p>The resource could not be found.</p>'
+# Set up database connection
+db_connector = sqlite3.connect("data.db")
+db_cursor = db_connector.cursor()
 
 @app.route('/', methods=['GET'])
-def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
-@app.route('/data', methods=['GET'])
-def data_all():
-    return flask.jsonify(some_data)
-
-@app.route('/data/id', methods=['GET'])
-def specific_data():
-    if 'id' in flask.request.args:
-        try:
-            id = int(flask.request.args['id'])
-        except ValueError:
-            return 'ERROR: Value "id" must be an integer'
-    else:
-        return 'ERROR: Value "id" must be set'
-    
-    results = []
-
-    for i in some_data:
-        if i['id'] == id:
-            results.append(i)
-    
-    return flask.jsonify(results)
-
-if "__name__" == "__main__":
-    app.run()
+def main_page():
+    a = "placeholder"
+    return a
